b11dcc2a5b854b7d2538ea6fe62c8949e59d7927
guille <gllinaresp@uoc.edu>
2024-01-15 07:22:04 +0100

    ADD rewards by proximity

--- d_shooting_minigrid/shooting_minigrid_env_v3.py
+++ d_shooting_minigrid/shooting_minigrid_env_v3.py
@@ -31,10 +31,70 @@ class ShootingMiniGridEnvV3(ShootingMiniGridEnvV2):
             shape=(view_size,),
             dtype="int16",
         )
+        # self.agent_target_distance = 0
+        # self.agent_target_angle_diff = 0
+
+    # def reset(
+    #         self,
+    #         *,
+    #         seed: int | None = None,
+    #         options: dict[str, Any] | None = None,
+    # ) -> tuple[ObsType, dict[str, Any]]:
+    #     obs, info = super().reset(seed=seed)
+    #     self.agent_target_distance = self.get_agent_target_distance()
+    #     self.agent_target_angle_diff = self.get_agent_target_angle_diff()
+    #     return obs, info
+
+    # def get_agent_target_distance(self):
+    #     return np.linalg.norm(np.array(self.agent_pos) - np.array(self.target_position))
+    #
+    # def get_agent_target_angle_diff(self):
+    #     # Calculate the vector from the agent to the object
+    #     target_vector = np.array(self.target_position) - np.array(self.agent_pos)
+    #     # Calculate the angle between the agent's direction and the object vector
+    #     angle_difference = -np.arctan2(target_vector[1], target_vector[0]) - self.agent_dir
+    #     return round(abs(angle_difference), 2)
 
     def step(self, action):
         # Same step as in ShootingMiniGridEnvV2
         return super().step(action)
+
+    # def step(self, action):
+    #     # Same step as in ShootingMiniGridEnvV2
+    #     obs, reward, terminated, truncated, info = super().step(action)
+    #     if not terminated:
+    #         if action in [91, 92, 93, 94]:
+    #             # Moving action
+    #             previous_distance = self.agent_target_distance
+    #             current_distance = self.get_agent_target_distance()
+    #             distance_diff = previous_distance - current_distance
+    #             if current_distance > 7:
+    #                 # Si se encuentra a menos de 5 celdas de distancia, no premiar el movimiento.
+    #                 if distance_diff > 0:
+    #                     reward = 0.1
+    #                 elif distance_diff < 0:
+    #                     reward = - 0.1
+    #             self.agent_target_distance = current_distance
+    #             self.agent_target_angle_diff = self.get_agent_target_angle_diff()
+    #
+    #         elif 0 <= action <= 90:
+    #             # Rotation
+    #             previous_angle = self.agent_target_angle_diff
+    #             current_angle = self.get_agent_target_angle_diff()
+    #             angle_diff = previous_angle - current_angle
+    #             if angle_diff > 0:
+    #                 reward = 0.1
+    #             elif angle_diff < 0:
+    #                 reward = - 0.1
+    #             self.agent_target_angle_diff = current_angle
+    #     return obs, reward, terminated, truncated, info
+
+    # def _reward(self) -> float:
+    #     """
+    #     Compute the reward to be given upon success
+    #     """
+    #
+    #     return (1 - 0.9 * (self.step_count / self.max_steps)) * 100
 
     def gen_obs(self):
         grid = self.grid
